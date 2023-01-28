d12b49de5a65ccff9c293afe2dc7cc813f6fe38a
amaury dav <amauryat@e-nautia.com>
2023-01-28 17:41:53 +0100

    indent

--- 00-Setup/00-Demo/lib/demo.rb
+++ 00-Setup/00-Demo/lib/demo.rb
@@ -1,9 +1,9 @@
 def circle_area(radius)
   # TODO: Implement the `circle_area` method
   area = 3.14 * radius ** 2
-if radius > 0
-  return area
-else
-  return 0
-end
+  if radius > 0
+    return area
+  else
+    return 0
+  end
 end
