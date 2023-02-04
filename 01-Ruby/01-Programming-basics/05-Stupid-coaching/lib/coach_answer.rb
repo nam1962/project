a35a53227cd1b4e7f0a46b3080310a025efe1f24
amaury dav <amauryat@e-nautia.com>
2023-02-04 10:52:15 +0100

    paas fini

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,27 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+
+  #get rid
+if I say "I am going to work right now!" exit
+
+# you ask her/him something
+if "?" at end of sentence
+
+  puts "Silly question, get dressed and go to work!"
+
+  loop
+
+# you don’t ask her/him something, but just tell her/him something
+else
+  puts "I don't care, get dressed and go to work!"
+
+loop
+
+
+
+
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
 end
-
