09d74d8873fdaac3be63860f3ba3f4b276d52d79
amaury dav <amauryat@e-nautia.com>
2023-02-11 16:56:03 +0100

    à reviser

--- 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,12 @@
 def array_to_hash(array)
   # TODO: implement the method :)
+  new_hash = {}
+  array.each_with_index do |first_name, index|
+    if block_given?
+      new_hash[yield(index)] = first_name
+    else
+      new_hash[index.to_s] = first_name
+    end
+  end
+  new_hash
 end
