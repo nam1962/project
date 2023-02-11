dc7c25794ad6d7d5ef01695a17e3f93e6f0db5d7
amaury dav <amauryat@e-nautia.com>
2023-02-11 12:21:19 +0100

    exo2 pénible

--- 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/restaurant_order.rb
+++ 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/restaurant_order.rb
@@ -1,4 +1,8 @@
+
+
+
 DISHES_CALORIES = {
+  "no_meal" => 0,
   "Hamburger" => 250,
   "Cheese Burger" => 300,
   "Veggie Burger" => 540,
@@ -6,13 +10,25 @@ DISHES_CALORIES = {
   "Sweet Potatoes" => 230,
   "Salad" => 15,
   "Iced Tea" => 70,
-  "Lemonade" => 90
+  "Lemonade" => 90,
 }
 
-def poor_calories_counter(burger, side, beverage)
+def poor_calories_counter(burger, side, beverage, meal = "no_meal")
   DISHES_CALORIES[burger] + DISHES_CALORIES[side] + DISHES_CALORIES[beverage]
 end
 
+
+
 def calories_counter(orders)
   # TODO: return number of calories for a less constrained order
+  DISHES_CALORIES["Cheesy Combo"] = poor_calories_counter("Cheese Burger", "Sweet Potatoes", "Lemonade")
+  DISHES_CALORIES["Veggie Combo"] = poor_calories_counter("Veggie Burger", "Sweet Potatoes", "Iced Tea")
+  DISHES_CALORIES["Vegan Combo"] = poor_calories_counter("Vegan Burger", "Salad", "Lemonade")
+  total_k = 0
+  orders.each do |order|
+  total_k += DISHES_CALORIES[order]
+  end
+  return total_k
 end
+
+p calories_counter([ "Hamburger","Cheesy Combo"])
