9523a8790ddbfcf4984831a95824d1c82e463e1c
amaury dav <amauryat@e-nautia.com>
2023-02-11 11:31:44 +0100

    good style

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/restaurant_order.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/restaurant_order.rb
@@ -1,20 +1,17 @@
+MENU = {
+  "no_choice" => 0,
+  "Hamburger" => 250,
+  "Cheese Burger" =>	300,
+  "Veggie Burger" =>	540,
+  "Vegan Burger" =>	350,
+  "Sweet Potatoes" =>	230,
+  "Salad" =>	15,
+  "Iced Tea" =>	70,
+  "Lemonade" =>	90
+}
+
 def poor_calories_counter(burger = "no_choice", side = "no_choice", beverage = "no_choice")
-
-  # TODO: return number of calories for this restaurant order
-# Créer menu avec calories en hash
-
-  menu = {
-    "no_choice" => 0,
-    "Hamburger" => 250,
-    "Cheese Burger" =>	300,
-    "Veggie Burger" =>	540,
-    "Vegan Burger" =>	350,
-    "Sweet Potatoes" =>	230,
-    "Salad" =>	15,
-    "Iced Tea" =>	70,
-    "Lemonade" =>	90
-  }
-  menu["#{burger}"] + menu["#{side}"] + menu["#{beverage}"]
+  MENU[burger] + MENU[side] + MENU[beverage]
 end
 
 puts poor_calories_counter("Hamburger")
