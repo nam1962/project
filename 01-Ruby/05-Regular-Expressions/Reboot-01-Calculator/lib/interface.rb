ab1d6e29a32ba8effddafd09cd8f19e9b91de0ae
amaury dav <amauryat@e-nautia.com>
2023-02-16 18:56:21 +0100

    reboot1

--- 01-Ruby/05-Regular-Expressions/Reboot-01-Calculator/lib/interface.rb
+++ 01-Ruby/05-Regular-Expressions/Reboot-01-Calculator/lib/interface.rb
@@ -1,1 +1,31 @@
 # TODO: you can build your calculator program here!
+# Dire bonjour à l'utilisateur
+require_relative "calculator"
+
+# Dire bonjour à l'utilisateur
+puts "Hello you !"
+# answer = "Y"
+
+while true
+  # Demande à l'utilisateur quel est le premier chiffre
+  puts "Enter a first number"
+  # Stocker le premier chiffre dans une variable
+  first_number = gets.chomp.to_i
+  # Demande à l'utilisateur un deuxieme chiffre
+  puts "Enter a second number"
+  # Stocke ce deuxieme chiffre dans une variable
+  second_number= gets.chomp.to_i
+  # Demande à l'utilisateur l'opérateur
+  puts "Choose operator [+][-][*][/]"
+  # Stocke cet operateur dans une variable
+  operator = gets.chomp
+  # On calcule
+  # resultat = nb1.method(operator).(nb2)
+  puts calculate(first_number, second_number, operator)
+
+  # Demander à l'utilisteur si il veut continuer ou pas
+  puts "Do you want to calculate again? [Y/N]"
+  # Lire sa reponse
+  answer = gets.chomp
+  break if answer == "N"
+end
