0584a15cc8191a2cc91455e9d15910fb0bea7a0f
amaury dav <amauryat@e-nautia.com>
2023-02-23 19:43:58 +0100

    à relire

--- 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/vending_machine.rb
+++ 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/vending_machine.rb
@@ -10,10 +10,20 @@ class VendingMachine
   def insert_coin(input_cents)
     # TODO: what happens to @snack_count, @user_balance_cents and @snack_price_cents
     # when the user inserts a coin?
+    @user_balance_cents += input_cents
   end
+
+  attr_reader :user_balance_cents, :snack_price_cents
+  attr_accessor :snack_count
 
   def buy_snack
     # TODO: what happens to @snack_count, @user_balance_cents and @snack_price_cents
     # when the user pushes a button to buy a Snack?
+    if snack_count.zero? || user_balance_cents <= snack_price_cents
+      @snack_count = snack_count
+    else
+      @snack_count -= 1
+      @user_balance_cents = user_balance_cents - snack_price_cents
+    end
   end
 end
