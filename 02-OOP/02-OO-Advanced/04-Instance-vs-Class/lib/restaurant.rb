4ba508c60e28e6a6c6220067e6c4fa00c0cf39cc
amaury dav <amauryat@e-nautia.com>
2023-02-25 15:40:21 +0100

    exo3

--- 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,22 @@
 class Restaurant
   # TODO: add relevant accessors if necessary
+  attr_reader :city, :name, :restaurants
+  attr_accessor :average_rating
 
   def initialize(city, name)
     # TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @ratings = []
+  end
+  # TODO: implement .filter_by_city and #rate methods
+
+  def rate(new_rate)
+    @ratings << new_rate
+    @average_rating = @ratings.sum.fdiv(@ratings.length)
   end
 
-  # TODO: implement .filter_by_city and #rate methods
+  def self.filter_by_city(restaurants, town)
+    restaurants.select { |restaurant| restaurant.city == town }
+  end
 end
