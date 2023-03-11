02615dae7ff7f60e0c785a5c6c623f9018ca136f
amaury dav <amauryat@e-nautia.com>
2023-03-11 10:50:44 +0100

    partial

--- 02-OOP/06-Food-Delivery-Day-Two/01-Food-Delivery/app/models/employee.rb
+++ 02-OOP/06-Food-Delivery-Day-Two/01-Food-Delivery/app/models/employee.rb
@@ -1,9 +1,20 @@
 class Employee
-  attr_accessor :id, :username, :password
+  attr_accessor :id, :username, :password, :role
 
   def initialize(attributes = {})
     @id = attributes[:id]
     @username = attributes[:username]
     @password = attributes[:password]
+    @role = attributes[:role]
+
   end
+
+def manager?
+ @role == "manager"
+end
+
+def rider?
+  @role == "rider"
+end
+
 end
