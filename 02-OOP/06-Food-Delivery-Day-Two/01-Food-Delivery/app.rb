e8c3f0f46c9e1c67a731289113a3e0a0f31d694f
amaury dav <amauryat@e-nautia.com>
2023-03-11 12:56:01 +0100

    login part1

--- 02-OOP/06-Food-Delivery-Day-Two/01-Food-Delivery/app.rb
+++ 02-OOP/06-Food-Delivery-Day-Two/01-Food-Delivery/app.rb
@@ -2,16 +2,23 @@ require_relative "app/controllers/meals_controller"
 require_relative "app/repositories/meal_repository"
 require_relative "app/controllers/customers_controller"
 require_relative "app/repositories/customer_repository"
+require_relative "app/controllers/sessions_controller"
+require_relative "app/repositories/employee_repository"
 require_relative "router"
 
 MEALS_CSV_FILE = File.join(__dir__, "data/meals.csv")
 CUSTOMERS_CSV_FILE = File.join(__dir__, "data/customers.csv")
+EMPLOYEE_CSV_FILE = File.join(__dir__, "data/employees.csv")
 meal_repository = MealRepository.new(MEALS_CSV_FILE)
 meals_controller = MealsController.new(meal_repository)
 
 customer_repository = CustomerRepository.new(CUSTOMERS_CSV_FILE)
 customers_controller = CustomersController.new(customer_repository)
-router = Router.new(meals_controller, customers_controller)
+
+employee_repository = EmployeeRepository.new(EMPLOYEE_CSV_FILE)
+sessions_controller = SessionsController.new(employee_repository)
+
+router = Router.new(meals_controller, customers_controller, sessions_controller)
 
 # Start the app
 router.run
--- 02-OOP/06-Food-Delivery-Day-Two/01-Food-Delivery/router.rb
+++ 02-OOP/06-Food-Delivery-Day-Two/01-Food-Delivery/router.rb
@@ -1,12 +1,15 @@
 # TODO: implement the router of your app.
 class Router
-  def initialize(meals_controller, customers_controller)
+
+  def initialize(meals_controller, customers_controller, sessions_controller)
     @meals_controller = meals_controller
     @customers_controller = customers_controller
+    @sessions_controller = sessions_controller
     @running = true
   end
 
   def run
+    employee = @sessions_controller.login
     while @running
       display_tasks
       action = gets.chomp.to_i
@@ -16,7 +19,45 @@ class Router
 
   private
 
+# def Login
+#   puts '*' * 30
+#   puts 'Welcome to the food delivery'
+#   puts '*' * 30
+#   puts "LOGIN"
+#   print "> "
+#   puts "username?"
+#   puts "password?"
+#   puts "Welcome #{username}"
+# end
+
+
   def display_tasks
+    puts '*' * 30
+    puts 'Welcome to the food delivery'
+    puts '*' * 30
+    puts "MENU"
+    puts "1 - List all meals"
+    puts "2 - Add a new meal"
+    puts "3 - List all customers"
+    puts "4 - Add a new customer"
+    puts "5 - Stop and exit the program"
+    print "> "
+  end
+
+  def display_manager_tasks
+    puts '*' * 30
+    puts 'Welcome to the food delivery'
+    puts '*' * 30
+    puts "MENU"
+    puts "1 - List all meals"
+    puts "2 - Add a new meal"
+    puts "3 - List all customers"
+    puts "4 - Add a new customer"
+    puts "5 - Stop and exit the program"
+    print "> "
+  end
+
+  def display_rider_tasks
     puts '*' * 30
     puts 'Welcome to the food delivery'
     puts '*' * 30
