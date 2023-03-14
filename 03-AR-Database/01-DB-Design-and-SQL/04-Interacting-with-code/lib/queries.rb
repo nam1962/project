db4bdaf156bb8b09514e7590e95d1c0bd902fe64
amaury dav <amauryat@e-nautia.com>
2023-03-14 22:09:06 +0100

    4th question

--- 03-AR-Database/01-DB-Design-and-SQL/04-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/04-Interacting-with-code/lib/queries.rb
@@ -2,20 +2,24 @@ require 'sqlite3'
 
 def artist_count(db)
   # TODO: use `db` to execute an SQL query against the database.
-  count = db.execute("SELECT COUNT(*) AS name FROM artists")
-  count[0][0]
+ db.execute("SELECT COUNT(*) AS name FROM artists").flatten.first
+ # db.execute("SELECT COUNT(*) AS name FROM artists")[0][0]
 end
 
 def number_of_rows(db, table_name)
   # TODO: count number of rows in table table_name
+  db.execute("SELECT COUNT(*) AS id FROM #{table_name}")[0][0]
 end
 
 def sorted_artists(db)
   # TODO: return array of artists' names sorted alphabetically
+  db.execute("SELECT name FROM artists ORDER BY name").flatten
 end
 
 def love_tracks(db)
   # TODO: return array of love songs' names
+  db.execute("SELECT name FROM tracks WHERE name LIKE '%love%'").flatten
+
 end
 
 def long_tracks(db, min_length)
