b3ac9c9350e2e2daa1f8b67d59c0fed20e6593fb
amaury dav <amauryat@e-nautia.com>
2023-03-16 21:50:10 +0100

    almost_done

--- 03-AR-Database/01-DB-Design-and-SQL/05-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/05-SQL-queries/lib/join_queries.rb
@@ -2,10 +2,31 @@ require 'sqlite3'
 
 def detailed_tracks(db)
   # TODO: return the list of tracks with their album and artist.
+  db.execute("SELECT tracks.name, albums.title, artists.name
+    FROM tracks
+    JOIN albums on tracks.album_id = albums.id
+    JOIN artists on albums.artist_id = artists.id"
+  )
+
 end
 
 def stats_on(db, genre_name)
+
+
   # TODO: For the given category of music, return the number of tracks and the average song length (as a stats hash)
+  stat = db.execute(%(
+    SELECT genres.name, COUNT(tracks.id), ROUND(AVG(milliseconds/60_000),2)
+    FROM tracks
+    INNER JOIN genres on tracks.genre_id = genres.id
+    WHERE genre.name = #{genre_name})
+  ).flatten
+
+return {
+category: stat[0],
+number_of_songs: stat[1],
+avg_length: stat[2]
+}
+
 end
 
 def top_five_artists(db, genre_name)
