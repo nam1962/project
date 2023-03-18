f52225a975d2d9a357ebd48c486445ee0fafe3c6
amaury dav <amauryat@e-nautia.com>
2023-03-18 10:31:19 +0100

    done

--- 03-AR-Database/02-SQL-CRUD/01-Query-the-DB/lib/query.rb
+++ 03-AR-Database/02-SQL-CRUD/01-Query-the-DB/lib/query.rb
@@ -3,4 +3,5 @@ require 'sqlite3'
 def all_artists(db)
   # TODO: use `db` to execute an SQL query against the database to
   # retrieve all columns from all records from the `artists` table.
+  db.execute("SELECT * FROM artists GROUP BY id, name")
 end
