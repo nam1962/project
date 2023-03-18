b9bf13c3b68a7b6a03766fe4af6aa50c21287f20
amaury dav <amauryat@e-nautia.com>
2023-03-18 15:07:15 +0100

    2 done

--- 03-AR-Database/02-SQL-CRUD/02-Read/app/models/post.rb
+++ 03-AR-Database/02-SQL-CRUD/02-Read/app/models/post.rb
@@ -14,18 +14,31 @@ class Post
     @votes = attributes[:votes]
   end
 
+  def self.build(post)
+    return unless post
+    Post.new(
+      {
+        id: post["id"],
+        title: post["title"],
+        url: post["url"],
+        votes: post["votes"]
+      }
+    )
+
+  end
+
   def self.find(id)
     DB.results_as_hash = true
     query = "SELECT * FROM posts WHERE id = ?"
     post = DB.execute(query, id).first
-    unless post.nil?
-      Post.new({id: post["id"],
-              title: post["title"],
-              url: post["url"],
-              votes: post["votes"]
-              })
-    end
+    # unless post.nil?
+    build(post)
+    # end
   end
 
-
+  def self.all
+  DB.results_as_hash = true
+    all_posts = DB.execute("SELECT * FROM posts")
+    all_posts.map {|post| build(post)}.compact
+  end
 end
