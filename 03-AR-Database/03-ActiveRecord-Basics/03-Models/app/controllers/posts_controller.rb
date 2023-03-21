47104a668b2394d432dea8c0a951da6895598c66
amaury dav <amauryat@e-nautia.com>
2023-03-21 21:44:49 +0100

    model done

--- 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 require_relative "../views/posts_view"
+require_relative "../models/post"
 
 class PostsController
   def initialize
@@ -9,25 +10,45 @@ class PostsController
     # DO NOT WRITE SQL QUERIES
     # TODO: gather all posts from the database
     # TODO: give them to the view to be printed
+    posts = Post.all
+    @view.display_all(posts)
   end
 
   def create
     # DO NOT WRITE SQL QUERIES
     # TODO: create a post
+    title = @view.ask_user_for("title")
+    url = @view.ask_user_for("url")
+    post = Post.create(title:, url:)
   end
 
   def update
     # DO NOT WRITE SQL QUERIES
     # TODO: update a post
+    index
+    id = @view.ask_user_for("id").to_i
+    post = Post.find(id)
+    post.title = @view.ask_user_for("new title")
+    post.url = @view.ask_user_for("new url")
+    post.save
   end
 
   def destroy
     # DO NOT WRITE SQL QUERIES
     # TODO: destroy a post
+    index
+    id = @view.ask_user_for("id").to_i
+    post = Post.find(id)
+    post.destroy
   end
 
   def upvote
     # DO NOT WRITE SQL QUERIES
     # TODO: increment the `votes` column for a post
+    index
+    id = @view.ask_user_for("id").to_i
+    post = Post.find(id)
+    post.votes += 1
+    post.save
   end
 end
--- 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
@@ -1,3 +1,19 @@
 class PostsView
-  # TODO: implement some methods here when controller needs to `puts` or `gets`
+  def ask_user_for(string)
+    puts "#{string}?"
+    print "> "
+    gets.chomp
+  end
+
+  def display_all(posts)
+    puts "---------------------------------"
+    puts "----------- All Posts -----------"
+    puts "---------------------------------"
+    puts ""
+    posts.each do |post|
+      puts "#{post.id} - #{post.title} (#{post.votes} vote(s))"
+    end
+    puts ""
+    puts "---------------------------------"
+  end
 end
