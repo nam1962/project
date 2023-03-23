7e51f794354d4abd6eeece249947cc323c79a0d6
amaury dav <amauryat@e-nautia.com>
2023-03-23 19:31:30 +0100

    pretty_fake

--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
@@ -1,4 +1,5 @@
 require "faker"
 # TODO: Write a seed to insert 100 posts in the database
-Post.create(title: Faker::Hacker.say_something_smart, url: Internet.url, votes: 2)
-
+100.times do
+  Post.create(title: Faker::Hacker.say_something_smart, url: Faker::Internet.url, votes: (1..500).to_a.sample)
+end
