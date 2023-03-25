7271467ffd6dc1fd9a57101eaa87fc70b9e9086d
amaury dav <amauryat@e-nautia.com>
2023-03-25 16:57:51 +0100

    before_bonus

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   # TODO: Add some validation
+  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5 }
+  validates :user, presence: true
+  validates :url, presence: true, format: { with: /https?:\/\/[\S]+/ }
 end
