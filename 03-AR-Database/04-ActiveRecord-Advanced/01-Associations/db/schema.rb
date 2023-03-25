c0c7cd8d12f20adc9ef51ae62a7a99aa017345f8
amaury dav <amauryat@e-nautia.com>
2023-03-25 11:51:21 +0100

    end_begin

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/schema.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2014_10_25_152200) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_25_112702) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.integer "votes", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "posts", "users"
 end
